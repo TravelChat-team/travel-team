1edbc55862ce9f202496d1684c6a2c8b956fa195
ig.basalt@gmail.com <ig.basalt@gmail.com>
2023-10-01 21:43:35 +0300

    server_start.sh

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,10 @@
-#!/usr/bin/env bash
-cd /home/ec2-user/server
-sudo java -jar -Dserver.port=80 \
-    *.jar > /dev/null 2> /dev/null < /dev/null &
+##!/usr/bin/env bash
+#cd /home/ec2-user/server
+#sudo java -jar -Dserver.port=80 \
+#    *.jar > /dev/null 2> /dev/null < /dev/null &
+#!/bin/bash
+source ~/.bashrc
+# To kill the process incase of redeployment
+kill -9 $(lsof -i :8080)
+cd /home/ec2-user/my-web-server/build/libs/
+nohup java -jar TravelChat.jar &> webserverlog.log &
